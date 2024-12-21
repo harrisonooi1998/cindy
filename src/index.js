6a155f758d9f94b51dbe356d13d4ed18ba100d31
Harrison ooi <harrisontooi@gmail.com>
2024-12-21 14:41:24 -0500

    deleted the nyekiss folder

--- src/index.js
+++ src/index.js
@@ -4,17 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom';
-
+import { HashRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
-
 
 reportWebVitals();
